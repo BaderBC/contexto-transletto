85c43e967a8923a59091afe3c1dade097029612a
Bartłomiej Strama <kontakt@bstrama.com>
2024-06-03 22:55:46 +0200

    anki integration added and breaking down PopupModal componenet

---
+++ src/components/PopupModal.svelte
@@ -0,0 +1,69 @@
+<script context="module" lang="ts">
+  export enum SectionToShow {
+    TRANSLATION,
+    ANKI_NOT_AVAILABLE
+  }
+</script>
+
+<script lang="ts">
+  import TranslationSection from './TranslationSection.svelte';
+  import Notification, { NotificationKind, NotificationType } from './Notification.svelte';
+  import { Sleep } from '../lib/utils';
+  import AnkiNotAvailable from './AnkiNotAvailable.svelte';
+
+  export let wordToTranslate: string;
+
+  let notification: NotificationType = null;
+  let sectionToShow = SectionToShow.TRANSLATION;
+
+  export function showNotification(title: string, content: string, kind: NotificationKind, duration_ms = 5000) {
+    notification = new NotificationType(title, content, kind, duration_ms);
+
+    Sleep.ms(duration_ms)
+      .then(() => notification = null);
+  }
+</script>
+
+<section id="card">
+  <div id="card-regular-content">
+    <header>Contexto transletto</header>
+    {#if (sectionToShow === SectionToShow.TRANSLATION)}
+      <TranslationSection {showNotification} {wordToTranslate} bind:sectionToShow />
+    {:else if (sectionToShow === SectionToShow.ANKI_NOT_AVAILABLE)}
+      <AnkiNotAvailable bind:sectionToShow />
+    {/if}
+  </div>
+
+  {#if (notification)}
+    <Notification {notification} />
+  {/if}
+</section>
+
+<style lang="scss">
+  @use "src/assets/css/variables";
+
+  header {
+    margin: 0;
+    padding: 0;
+    font-size: 1.5rem;
+    font-weight: 1000;
+    color: #333;
+  }
+
+  #card-regular-content {
+    padding: 15px;
+  }
+
+  #card-regular-content {
+    background: variables.$popup-modal-bg-color;
+  }
+
+  #card {
+    border-radius: 16px;
+    overflow: hidden;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.3);
+    backdrop-filter: blur(7px);
+    -webkit-backdrop-filter: blur(7px);
+    border: 2px solid variables.$popup-modal-bg-color;
+  }
+</style>
---
+++ src/lib/anki.ts
@@ -0,0 +1,82 @@
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function invoke(action, version, params = {}): Promise<Object> {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('error', () => reject('failed to issue request'));
+    xhr.addEventListener('load', () => {
+      try {
+        const response = JSON.parse(xhr.responseText);
+        if (Object.getOwnPropertyNames(response).length != 2) {
+          throw 'response has an unexpected number of fields';
+        }
+        // eslint-disable-next-line no-prototype-builtins
+        if (!response.hasOwnProperty('error')) {
+          throw 'response is missing required error field';
+        }
+        // eslint-disable-next-line no-prototype-builtins
+        if (!response.hasOwnProperty('result')) {
+          throw 'response is missing required result field';
+        }
+        if (response.error) {
+          throw response.error;
+        }
+        resolve(response.result);
+      } catch (e) {
+        reject(e);
+      }
+    });
+
+    xhr.open('POST', 'http://127.0.0.1:8765');
+    xhr.send(JSON.stringify({ action, version, params }));
+  });
+}
+
+export async function ensureDeckIsCreated(foreignLanguage: string) {
+  await invoke('createDeck', 6, {
+    deck: `contexto-transletto::${foreignLanguage}`,
+  });
+}
+
+export class ContextoTranslettoSentence {
+  private readonly leftSide: string;
+  private readonly selectedPhrase: string;
+  private readonly rightSide: string;
+
+  constructor(leftSide: string, selectedPhrase: string, rightSide: string) {
+    this.leftSide = leftSide.trim();
+    this.selectedPhrase = selectedPhrase.trim();
+    this.rightSide = rightSide.trim();
+  }
+
+  public joinMakingSelectedPhraseBold() {
+    return `${this.leftSide} <b>${this.selectedPhrase}</b> ${this.rightSide}`;
+  }
+}
+
+export async function addTranslationToDeck(
+  sentenceToTranslate: ContextoTranslettoSentence,
+  sentenceTranslation: ContextoTranslettoSentence,
+  foreignLanguage: string,
+) {
+  await ensureDeckIsCreated(foreignLanguage);
+  await invoke('addNote', 6, {
+    note: {
+      deckName: `contexto-transletto::${foreignLanguage}`,
+      modelName: 'Basic',
+      fields: {
+        Front: `Translate: <br>${sentenceToTranslate.joinMakingSelectedPhraseBold()}`,
+        Back: `Correct translation: <br>${sentenceTranslation.joinMakingSelectedPhraseBold()}`,
+      },
+    },
+  });
+}
+
+export async function isAnkiAvailable() {
+  try {
+    await invoke('version', 6);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
