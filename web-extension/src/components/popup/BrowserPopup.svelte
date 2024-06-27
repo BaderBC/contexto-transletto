9ad67125f2a419791b4125d5592cd87720ddc159
Bartłomiej Strama <kontakt@bstrama.com>
2024-06-27 19:54:36 +0200

    settings popup implemented

---
+++ web-extension/src/components/popup/BrowserPopup.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import SettingsSection from '../settings/SettingsSection.svelte';
+</script>
+
+<main>
+  <SettingsSection />
+</main>
+
+<style>
+  main {
+      width: 300px;
+  }
+</style>
